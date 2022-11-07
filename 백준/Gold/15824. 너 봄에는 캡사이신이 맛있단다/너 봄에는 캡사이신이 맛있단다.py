11d89b2b6b8698fe8e302a45f6e352a91a1dfa7f
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-11-07 13:58:39 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 520 ms, Memory: 64820 KB -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,22 +1,12 @@
-def fast_pow(a, b):
-    # calc a^b fast!!
-    if b == 0:
-        return 1
-    temp = fast_pow(a, b // 2)
-    if b % 2 == 1:
-        return temp * temp * a % MOD
-    return temp * temp % MOD
-
-
 N = int(input())
 arr = sorted(map(int, input().split()))
 MOD = 1_000_000_007
-answer = 0
-for i, num in enumerate(arr):
-    # num이 최소가 되는 집합의 개수
-    max_set_n = fast_pow(2, i) % MOD
-    # num이 최대가 되는 집합의 개수
-    min_set_n = fast_pow(2, N - 1 - i) % MOD
-    answer = (answer + (max_set_n * num % MOD) - (min_set_n * num % MOD)) % MOD
+pow_list = [1]
+for _ in range(N):
+    pow_list.append(pow_list[-1] * 2 % MOD)
 
+answer = 0
+for i in range(N):
+    answer = (answer + (pow_list[i] - pow_list[N - i - 1]) * arr[i]) % MOD
+    
 print(answer)
