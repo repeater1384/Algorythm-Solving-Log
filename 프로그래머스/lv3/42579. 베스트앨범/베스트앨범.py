888539ead744dc7120e09cb5b17aa0826e627211
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-11-18 00:05:31 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,14 +1,7 @@
 def solution(genres, plays):
-    answer = []
-    
     freq = {}
     for g,p,num in zip(genres,plays,range(10001)):
         if freq.get(g) == None:freq[g] = [(p,num)]
         else:freq[g].append((p,num))
-        num += 1
         
-    for _ ,v in sorted(freq.items(),key = lambda x : -sum([y[0] for y in x[1]])):
-        for _,a in sorted(v,key=lambda x:-x[0])[:2]:
-            answer.append(a)
-    
-    return answer
+    return [a for _ ,v in sorted(freq.items(),key = lambda x : -sum([y[0] for y in x[1]])) for _,a in sorted(v,key=lambda x:-x[0])[:2]]
