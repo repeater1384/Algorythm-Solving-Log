317b8a6172f50bd0036f16fe257695c8e1c14b36
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-11-20 00:22:52 +0900

    [unrated] Title: 숫자 타자 대회, Time: 1950.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136797. 숫자 타자 대회/숫자 타자 대회.py
+++ 프로그래머스/unrated/136797. 숫자 타자 대회/숫자 타자 대회.py
@@ -1,29 +1,18 @@
 def solution(numbers):
-    answer = 0
     pos_arr = [[3,1],[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
-    
+    INF = 10**10
     wei = {(4,6):0,(6,4):0}
     for num in map(int,numbers):
         next_wei = {}
         for k,v in wei.items():
             a,b = k
             if a == num or b == num:
-                if (a,b) in next_wei:
-                    next_wei[(a,b)] = min(next_wei[(a,b)],v+1)
-                else:
-                    next_wei[(a,b)] = v+1
+                next_wei[(a,b)] = min(next_wei.get((a,b),INF),v+1)
             else:
                 dis_from_a = calc_dis(*pos_arr[num],*pos_arr[a]) + v
                 dis_from_b = calc_dis(*pos_arr[num],*pos_arr[b]) + v
-                if (a,num) in next_wei:
-                    next_wei[(a,num)] = min(next_wei[(a,num)], dis_from_b)
-                else:
-                    next_wei[(a,num)] = dis_from_b
-                    
-                if (num,b) in next_wei:
-                    next_wei[(num,b)] = min(next_wei[(num,b)],dis_from_a)
-                else:
-                    next_wei[(num,b)] = dis_from_a
+                next_wei[(a,num)] = min(next_wei.get((a,num),INF),calc_dis(*pos_arr[num],*pos_arr[b]) + v)
+                next_wei[(b,num)] = min(next_wei.get((b,num),INF),calc_dis(*pos_arr[num],*pos_arr[a]) + v)
         wei = next_wei
 
     return min(wei.values())
@@ -31,8 +20,6 @@ def solution(numbers):
 
 
 def calc_dis(y,x,i,j):
-    if y == i and x == j:
-        return 1
     y_diff = abs(y-i)
     x_diff = abs(x-j)
     if y_diff == x_diff:
