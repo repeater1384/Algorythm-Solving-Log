bed2a6762f4be9ab22f020f10452f606f63c2a91
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-12-05 12:21:09 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 3052 ms, Memory: 182488 KB -BaekjoonHub

---
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
@@ -0,0 +1,76 @@
+import sys
+
+input = sys.stdin.readline
+
+
+class SegmentTree:
+    def __init__(self, arr, N):
+        self.arr = arr
+        self.tree = [0] * (N * 4)
+        self.lazy = [0] * (N * 4)
+        self.init_tree(1, 1, N)
+
+    def init_tree(self, node, start, end):
+        if start == end:
+            self.tree[node] = self.arr[start]
+        else:
+            mid = (start + end) // 2
+            left = self.init_tree(node * 2, start, mid)
+            right = self.init_tree(node * 2 + 1, mid + 1, end)
+            self.tree[node] = left + right
+        return self.tree[node]
+
+    def query(self, node, start, end, left, right):
+        self.propagate(node, start, end)
+        # [left, right] 구간을 구함. left, right는 고정
+        if right < start or end < left:
+            return 0
+        if left <= start and end <= right:
+            return self.tree[node]
+
+        mid = (start + end) // 2
+        temp1 = self.query(node * 2, start, mid, left, right)
+        temp2 = self.query(node * 2 + 1, mid + 1, end, left, right)
+        return temp1 + temp2
+
+    def update_range(self, node, start, end, left, right, added):
+        self.propagate(node, start, end)
+        # [left, right] 구간에 added를 더함
+        if right < start or end < left:
+            pass
+        elif left <= start and end <= right:
+            self.tree[node] += (end - start + 1) * added
+            if start != end:
+                self.lazy[node * 2] += added
+                self.lazy[node * 2 + 1] += added
+        else:
+            mid = (start + end) // 2
+            temp1 = self.update_range(node * 2, start, mid, left, right, added)
+            temp2 = self.update_range(node * 2 + 1, mid + 1, end, left, right, added)
+            self.tree[node] = temp1 + temp2
+        return self.tree[node]
+
+    def propagate(self, node, start, end):
+        if self.lazy[node] == 0:
+            return
+
+        self.tree[node] += (end - start + 1) * self.lazy[node]
+        if start != end:
+            self.lazy[node * 2] += self.lazy[node]
+            self.lazy[node * 2 + 1] += self.lazy[node]
+        self.lazy[node] = 0
+
+
+N, M, K = map(int, input().split())
+arr = [0] + [int(input()) for _ in range(N)]
+tree = SegmentTree(arr, N)
+answer = []
+for _ in range(M + K):
+    v, *info = map(int, input().split())
+    if v == 1:
+        a, b, c = info
+        tree.update_range(1, 1, N, a, b, c)
+    if v == 2:
+        a, b = info
+        answer.append(tree.query(1, 1, N, a, b))
+print(*answer, sep='\n')
