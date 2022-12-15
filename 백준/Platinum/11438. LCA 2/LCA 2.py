f85519480662d5d4be2af4b7b9d235d0f2f3761d
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-12-15 14:10:16 +0900

    [Platinum V] Title: LCA 2, Time: 704 ms, Memory: 355228 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.py
+++ 백준/Platinum/11438. LCA 2/LCA 2.py
@@ -1,7 +1,8 @@
 import sys
 
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 5)
 input = sys.stdin.readline
+
 N = int(input())
 LOG = 21
 adj_list = [[] for _ in range(N + 1)]
@@ -11,31 +12,24 @@ for _ in range(N - 1):
     adj_list[b].append(a)
 
 depth = [-1] * (N + 1)
-visited = [False] * (N + 1)
 parents = [[-1] * LOG for _ in range(N + 1)]
 
 
 def dfs(cur, d):
-    visited[cur] = True
     depth[cur] = d
     for next in adj_list[cur]:
-        if visited[next]:
-            continue
-        parents[next][0] = cur
-        dfs(next, d + 1)
+        if depth[next] == -1:
+            parents[next][0] = cur
+            dfs(next, d + 1)
 
 
-def set_parents():
-    # 2^0 번째 조상 세팅
-    dfs(1, 0)
+# 2^0 번째 조상 세팅
+dfs(1, 0)
 
-    # 2^i 번째 조상 세팅
-    for i in range(1, LOG):
-        for j in range(1, N + 1):
-            parents[j][i] = parents[parents[j][i - 1]][i - 1]
-
-
-set_parents()
+# 2^i 번째 조상 세팅
+for i in range(1, LOG):
+    for j in range(1, N + 1):
+        parents[j][i] = parents[parents[j][i - 1]][i - 1]
 
 
 def lca(a, b):
@@ -43,16 +37,16 @@ def lca(a, b):
     if depth[a] > depth[b]:
         a, b = b, a
 
-    # b가 거슬러 올라와야 하는 양
+    # need = b가 거슬러 올라와야 하는 양
     need = depth[b] - depth[a]
     for i in range(LOG - 1, -1, -1):
         if need >= (1 << i):
             b = parents[b][i]
             need -= 1 << i
-
+    # 이미 같은 노드에 있다면.
     if a == b:
         return a
-
+    # 한칸 직전까지 올려줌
     for i in range(LOG - 1, -1, -1):
         if parents[a][i] != parents[b][i]:
             a = parents[a][i]
