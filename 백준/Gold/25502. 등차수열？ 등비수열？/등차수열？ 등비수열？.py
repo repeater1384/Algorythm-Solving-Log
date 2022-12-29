ad90554a0c7c971f7af1a444f0ddea45cd24d384
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2022-12-29 23:12:21 +0900

    [Gold IV] Title: 등차수열? 등비수열?, Time: 1576 ms, Memory: 221556 KB -BaekjoonHub

--- 백준/Gold/25502. 등차수열？ 등비수열？/등차수열？ 등비수열？.py
+++ 백준/Gold/25502. 등차수열？ 등비수열？/등차수열？ 등비수열？.py
@@ -14,43 +14,25 @@ for i in range(N - 1):
 answer = []
 for _ in range(M):
     i, v = map(int, input().split())
-    i -= 1
-    if i != 0:
-        diff = arr[i] - arr[i - 1]
-        rate = arr[i] / arr[i - 1]
-        if diff_dict[diff] == 1:
-            diff_dict.pop(diff)
-        else:
-            diff_dict[diff] -= 1
-        if rate_dict[rate] == 1:
-            rate_dict.pop(rate)
-        else:
-            rate_dict[rate] -= 1
-
-    if i != N - 1:
-        diff = arr[i + 1] - arr[i]
-        rate = arr[i + 1] / arr[i]
-        if diff_dict[diff] == 1:
-            diff_dict.pop(diff)
-        else:
-            diff_dict[diff] -= 1
-        if rate_dict[rate] == 1:
-            rate_dict.pop(rate)
-        else:
-            rate_dict[rate] -= 1
-
-    arr[i] = v
-
-    if i != 0:
-        diff = arr[i] - arr[i - 1]
-        rate = arr[i] / arr[i - 1]
-        diff_dict[diff] = diff_dict.get(diff, 0) + 1
-        rate_dict[rate] = rate_dict.get(rate, 0) + 1
-    if i != N - 1:
-        diff = arr[i + 1] - arr[i]
-        rate = arr[i + 1] / arr[i]
-        diff_dict[diff] = diff_dict.get(diff, 0) + 1
-        rate_dict[rate] = rate_dict.get(rate, 0) + 1
+    for j in range(i - 1, i + 1):
+        if 0 < j < N:
+            diff = arr[j] - arr[j - 1]
+            rate = arr[j] / arr[j - 1]
+            if diff_dict[diff] == 1:
+                diff_dict.pop(diff)
+            else:
+                diff_dict[diff] -= 1
+            if rate_dict[rate] == 1:
+                rate_dict.pop(rate)
+            else:
+                rate_dict[rate] -= 1
+    arr[i - 1] = v
+    for j in range(i - 1, i + 1):
+        if 0 < j < N:
+            diff = arr[j] - arr[j - 1]
+            rate = arr[j] / arr[j - 1]
+            diff_dict[diff] = diff_dict.get(diff, 0) + 1
+            rate_dict[rate] = rate_dict.get(rate, 0) + 1
 
     if len(diff_dict.keys()) == 1 and arr[1] > arr[0]:
         answer.append('+')
