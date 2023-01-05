c56e8daff489081501f043e2719d2db5279cff4d
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-01-05 13:11:24 +0900

    [Platinum IV] Title: 휴대폰 자판, Time: 1880 ms, Memory: 276592 KB -BaekjoonHub

--- 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.py
+++ 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.py
@@ -1,23 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+
+
 class Trie:
     def __init__(self):
-        self.children = {}
+        self.children = [None for _ in range(26)]
+        self.children_num = 0
         self.is_end = False
+
+
+def ctoi(c):
+    return ord(c) - ord('a')
 
 
 def insert(root, word):
     cur = root
-    for w in word:
-        if w not in cur.children:
+    for w in map(lambda x: ctoi(x), word):
+        if not cur.children[w]:
             cur.children[w] = Trie()
+            cur.children_num += 1
         cur = cur.children[w]
     cur.is_end = True
 
 
 def check(root, word):
     cnt = 1
-    cur = root.children[word[0]]
-    for w in word[1:]:
-        if cur.is_end or len(cur.children.keys()) > 1:
+    cur = root.children[ctoi(word[0])]
+    for w in [*map(lambda x: ctoi(x), word)][1:]:
+        if cur.is_end or cur.children_num > 1:
             cnt += 1
         cur = cur.children[w]
     return cnt
@@ -25,16 +36,15 @@ def check(root, word):
 
 while True:
     try:
-        N = int(input())
-    except EOFError:
+        N = int(input().strip())
+    except:
         break
 
     root = Trie()
-    words = [input() for _ in range(N)]
+    words = [input().strip() for _ in range(N)]
     for word in words:
         insert(root, word)
-    avg = 0
-    for word in words:
-        cnt = check(root, word)
-        avg += cnt
-    print(f'{(avg / N):.2f}')
+
+    temp = [check(root, word) for word in words]
+    avg = sum(temp) / N
+    print(f'{avg:.2f}')
