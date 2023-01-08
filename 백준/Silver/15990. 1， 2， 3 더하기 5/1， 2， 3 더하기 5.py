0a3781716cb995acb5ea12fb7e07da989fcfda07
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-01-08 20:06:20 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 168 ms, Memory: 50724 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -6,7 +6,6 @@ input = sys.stdin.readline
 dp = [[0] * 4 for _ in range(100001)]
 MOD = 1_000_000_009
 
-arr = [int(input()) for _ in range(int(input()))]
 dp[1][1] = 1
 dp[2][2] = 1
 dp[3][3] = 1
@@ -17,4 +16,5 @@ for n in range(4, 100001):
     dp[n][1] = (dp[n - 1][2] + dp[n - 1][3]) % MOD
     dp[n][2] = (dp[n - 2][1] + dp[n - 2][3]) % MOD
     dp[n][3] = (dp[n - 3][1] + dp[n - 3][2]) % MOD
-print(*[sum(dp[n]) % MOD for n in arr], sep='\n')
+    
+print(*[sum(dp[n]) % MOD for n in [int(input()) for _ in range(int(input()))]], sep='\n')
