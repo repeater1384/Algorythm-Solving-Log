d05b6824e1ce5bd84a57b8c67d701d520696f00a
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-01-08 13:13:33 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 168 ms, Memory: 50732 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+# dp[n][i] -> n을 i를 마지막으로 해서 표현하는 가짓수
+dp = [[0] * 4 for _ in range(100001)]
+MOD = 1_000_000_009
+
+arr = [int(input()) for _ in range(int(input()))]
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][3] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+
+for n in range(4, 100001):
+    dp[n][1] = (dp[n - 1][2] + dp[n - 1][3]) % MOD
+    dp[n][2] = (dp[n - 2][1] + dp[n - 2][3]) % MOD
+    dp[n][3] = (dp[n - 3][1] + dp[n - 3][2]) % MOD
+print(*[sum(dp[n]) % MOD for n in arr], sep='\n')
