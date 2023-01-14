c4db591305a731fa50b0658f7ad26c92e3389234
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-01-14 17:37:30 +0900

    [Silver III] Title: 등수 매기기, Time: 452 ms, Memory: 52088 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+arr.sort()
+answer = 0
+for i in range(N):
+    answer += abs(arr[i] - i - 1)
+print(answer)
