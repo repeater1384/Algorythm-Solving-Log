40ddf82f599b6a90397389fc833626530545692f
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-03-31 17:42:14 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 236 ms, Memory: 25776 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		long cur_sum = 0;
+		for (int i = 0; i < M; i++)
+			cur_sum += arr[i];
+
+		long answer = 0;
+		for (int i = M; i <= N; i++) {
+			answer = Math.max(answer, cur_sum);
+			cur_sum = cur_sum + arr[i] - arr[i - M];
+		}
+		System.out.println(answer);
+	}
+
+}
