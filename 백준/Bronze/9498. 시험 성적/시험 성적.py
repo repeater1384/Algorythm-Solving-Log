e583258216828674ba48f167cb9ac6a6d20559d2
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-03 12:54:58 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if 90 <= score <= 100:
+    print('A')
+if 80 <= score <= 89:
+    print('B')
+if 70 <= score <= 79:
+    print('C')
+if 60 <= score <= 69:
+    print('D')
+if 0 <= score <= 59:
+    print('F')
