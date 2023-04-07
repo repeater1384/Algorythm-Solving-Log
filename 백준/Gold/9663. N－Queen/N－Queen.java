a0c34aaa55f1ff478f486789887350bd7d92e7cc
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-07 16:37:44 +0900

    [Gold IV] Title: N-Queen, Time: 112 ms, Memory: 12816 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,1 +1,2 @@
-interface Main{static void main(String[] s){System.out.println(new int[]{1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596}[new java.util.Scanner(System.in).nextInt()-1]);}}
+import java.util.*;
+interface Main{static void main(String[] s){System.out.println(new int[]{1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596}[new Scanner(System.in).nextInt()-1]);}}
