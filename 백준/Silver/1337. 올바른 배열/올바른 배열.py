1cdd7fd5111067539cae2898a7ce9deea12ab050
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-12 17:43:43 +0900

    [Silver IV] Title: 올바른 배열, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1337. 올바른 배열/올바른 배열.py
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -1,11 +1,8 @@
-arr = set([int(input()) for _ in range(int(input()))])
+arr = set(int(input()) for _ in range(int(input())))
 
 answer = float('inf')
 for a in arr:
     for i in range(-4, 1):
-        temp = 0
-        for k in range(5):
-            if a + i + k not in arr:
-                temp += 1
-        answer = min(answer, temp)
+        target = set(a + i + k for k in range(5))
+        answer = min(answer, len(target - arr))
 print(answer)
