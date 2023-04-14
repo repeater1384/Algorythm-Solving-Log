9ba75291f855881bd99cd1169e4ddfbf1a31cb57
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-14 17:16:37 +0900

    [Silver II] Title: 최소 힙, Time: 180 ms, Memory: 34304 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,12 @@
+import sys
+import heapq
+heap = []
+for _ in range(int(input())):
+    cmd = int(sys.stdin.readline())
+    if cmd:#삽입
+        heapq.heappush(heap,cmd)
+    else:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
