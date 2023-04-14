018a21e056f13e4111ab7268fe92b7414f34fcd0
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-14 16:35:12 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 72 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.py
@@ -0,0 +1,26 @@
+def matrix_mul_2_2(a, b):
+    _result = [[0] * 2 for _ in range(2)]
+
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                _result[i][j] += a[i][k] * b[k][j]
+
+    for i in range(2):
+        for j in range(2):
+            _result[i][j] %= 1_000_000_007
+
+    return _result
+
+
+n = (int(input()) // 2 * 2) + 1
+A = [[1, 1], [1, 0]]
+result = [[1, 0], [0, 1]]
+
+while n:
+    if n & 1:
+        result = matrix_mul_2_2(result, A)
+    A = matrix_mul_2_2(A, A)
+    n //= 2
+
+print(result[1][0] - 1)
