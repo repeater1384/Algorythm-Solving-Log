38823731da78245cb04b98396c658ee049f629b8
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-14 16:52:00 +0900

    [Gold II] Title: 트리의 지름, Time: 6776 ms, Memory: 75472 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+n = int(input())
+if n == 1:
+    print(0)
+    sys.exit(0)
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(n):
+    a, *info, _ = map(int, input().split())
+    for i in range(0,len(info),2):
+        b, w = info[i], info[i + 1]
+        graph[a].append((b, w))
+
+
+def get_farthest_node(start):
+    max_dist = 0
+    max_node = None
+
+    queue = deque([(start, 0)])
+    visited = [False] * (n + 1)
+    visited[start] = True
+
+    while queue:
+        cur, dist = queue.popleft()
+        for next, weight in graph[cur]:
+            if not visited[next]:
+                visited[next] = True
+                queue.append([next, dist + weight])
+                if max_dist < dist + weight:
+                    max_dist = dist + weight
+                    max_node = next
+    return max_node, max_dist
+
+
+farthest_node, _ = get_farthest_node(1)
+_, answer = get_farthest_node(farthest_node)
+
+print(answer)
