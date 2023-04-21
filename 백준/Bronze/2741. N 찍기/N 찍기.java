2185528ddc4d203a9ca54a3f141502cdd6506b1b
repeater1384 <43928426+repeater1384@users.noreply.github.com>
2023-04-21 14:08:11 +0900

    [Bronze V] Title: N 찍기, Time: 696 ms, Memory: 33892 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String args[]){
+    
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+
+        for(int i = 1 ;i<=a;i++){
+            System.out.println(i);
+        }        
+}
+}
